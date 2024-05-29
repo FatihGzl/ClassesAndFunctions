24fe923932b87cc35722b60d0ce469b266eaaee1
FatihGzl <f.al-guzel-51@hotmail.com>
2024-05-30 01:27:17 +0300

    ClassesAndFunctions committed.

---
+++ app/src/main/java/com/fatihalpgzl/classesandfunctions/Simpson.kt
@@ -0,0 +1,37 @@
+package com.fatihalpgzl.classesandfunctions
+
+//Primary Constructor
+class Simpson(var name : String, var age : Int, var job : String) {
+
+    private var height = 0
+
+    fun setHeight(num : Int) {
+        if (num > 100) {
+            height = num
+        }
+    }
+
+
+    /*
+    //Property
+    var name = ""
+    var age = 0
+    var job = ""
+    var weight = 0
+
+
+    //Secondary Constructor
+    constructor(name : String, age : Int, job : String, weight : Int, height : Int) {
+        this.name = name
+        this.age = age
+        this.job = job
+        this.weight = weight
+        this.height = height
+    }
+    */
+
+
+
+
+
+}
